8882d3a3d601f5ebc07a5cf051e9eb17a05ed5de
Juan Bautista Medeot <104040536+juanmedeot@users.noreply.github.com>
2026-05-02 21:03:39 -0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,4 +1,28 @@
+/* 
+=====================================================
+ Bronze Layer tables creation
+=====================================================
 
+ PURPOSE
+ This script creates tables in the Bronze layer.
+
+ -WHAT THIS SCRIPT DOES
+
+ Creates 3 Bronze tables (if they do not exist):
+    - bronze.yelp_business
+    - bronze.yelp_review
+    - bronze.yelp_user
+
+    Each table contains:
+      - id (BIGSERIAL primary key)
+      - raw_json (JSONB, raw record)
+      - ingested_at (timestamp of ingestion)
+
+ -REQUIREMENTS
+    -init.sql script must be run previously
+
+ =====================================================
+*/
 
 
 
