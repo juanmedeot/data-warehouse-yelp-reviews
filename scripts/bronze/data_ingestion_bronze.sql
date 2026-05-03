ebd1295ef847f3d78d1c8c64938eaf7d10b47056
Juan Bautista Medeot <104040536+juanmedeot@users.noreply.github.com>
2026-05-02 21:11:05 -0300

    Update data_ingestion_bronze.sql

--- scripts/bronze/data_ingestion_bronze.sql
+++ scripts/bronze/data_ingestion_bronze.sql
@@ -69,10 +69,10 @@ Bronze Layer Ingestion Script
 SET synchronous_commit = OFF;
 
 TRUNCATE TABLE bronze.yelp_business;
-\copy bronze.yelp_business(raw_json) FROM 'BUSINESS_FILE' WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8' );
+\copy bronze.yelp_business(raw_json) FROM <BUSINESS_FILE> WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8');
 
 TRUNCATE TABLE bronze.yelp_review;
-\copy bronze.yelp_review(raw_json) FROM 'REVIEW_FILE' WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8');
+\copy bronze.yelp_review(raw_json) FROM <REVIEW_FILE> WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8');
 
 TRUNCATE TABLE bronze.yelp_user;
-\copy bronze.yelp_user(raw_json) FROM 'USER_FILE' WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8');
+\copy bronze.yelp_user(raw_json) FROM <USER_FILE> WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8');
