3a737390f3339568da0c1c1b9abfcf0f6ae293f3
Juan Bautista Medeot <104040536+juanmedeot@users.noreply.github.com>
2026-05-02 21:05:40 -0300

    Update data_ingestion_bronze.sql

--- scripts/bronze/data_ingestion_bronze.sql
+++ scripts/bronze/data_ingestion_bronze.sql
@@ -1,1 +1,12 @@
 
+
+SET synchronous_commit = OFF;
+
+TRUNCATE TABLE bronze.yelp_business;
+\copy bronze.yelp_business(raw_json) FROM 'C:/Users/juanm/Documents/Diplomatura Ciencia de datos/SQL/SQL Project/Datasets/Yelp JSON/yelp_academic_dataset_business.json' WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8' );
+
+TRUNCATE TABLE bronze.yelp_review;
+\copy bronze.yelp_review(raw_json) FROM 'C:/Users/juanm/Documents/Diplomatura Ciencia de datos/SQL/SQL Project/Datasets/Yelp JSON/yelp_academic_dataset_review.json' WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8');
+
+TRUNCATE TABLE bronze.yelp_user;
+\copy bronze.yelp_user(raw_json) FROM 'C:/Users/juanm/Documents/Diplomatura Ciencia de datos/SQL/SQL Project/Datasets/Yelp JSON/yelp_academic_dataset_user.json' WITH (FORMAT csv, DELIMITER E'\x01', QUOTE E'\x02', ESCAPE E'\x03', ENCODING 'UTF8');
